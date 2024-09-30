9447664d3f259aef9389cf77510cc6073e21d47e
Nicolas <nicolasfigueiredo381@gmail.com>
2024-09-30 14:24:19 -0300

    Adicionar arquivos de projeto.

---
+++ Investimento.cs
@@ -0,0 +1,40 @@
+﻿namespace TesteMesa3
+{
+    public class Investimento
+    {
+        public double capitalInicial { get; set; }
+        public double taxaJurosDiaria { get; set; }
+        public DateTime inicioInvestimento { get; set; }
+        public DateTime fimInvestimento { get; set; }
+        public DateTime dataAtual;
+        public double rendimentoTotal;
+        public double rendimentoDiario;
+        public double taxaTotal;
+
+        public Investimento(double capital, double taxa, DateTime comecoInvestimento, DateTime finalInvestimento)
+        {
+            capitalInicial = capital;
+            taxaJurosDiaria = taxa;
+            inicioInvestimento = comecoInvestimento;
+            fimInvestimento = finalInvestimento;
+        }
+
+        public void Investir()
+        {
+            while (inicioInvestimento < fimInvestimento)
+            {
+                taxaTotal = Math.Pow1 + taxaJurosDiaria / 100, 30) - 1;
+                rendimentoDiario = capitalInicial * taxaTotal;
+                rendimentoTotal += rendimentoDiario;
+                inicioInvestimento = inicioInvestimento.AddDays(1);
+            }
+            Console.WriteLine(rendimentoTotal);
+        }
+
+        public void MostrarInvestimentoFuturo()
+        {
+            Console.WriteLine("Investimento feito com sucesso!");
+            Console.WriteLine($"R${rendimentoTotal:F2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿namespace TesteMesa3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine();
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("");
+
+            Console.WriteLine(new string('-', 25));
+            Console.WriteLine("Escolha uma das opções abaixo para mexer com seus investimentos: ");
+            Console.WriteLine(new string('-', 25));
+            Console.WriteLine("Sair do sistema - 0");
+            Console.WriteLine("Iniciar um investimento - 1");
+            Console.WriteLine(new string('-', 25));
+
+            int escolha = int.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 0:
+                    Environment.Exit(1);
+                    break;
+                case 1:
+                    InstanciarInvestimento();
+                    Console.ReadKey();
+                    Menu();
+                    break;;
+            }
+        }
+
+        static Investimento InstanciarInvestimento()
+        {
+            Console.WriteLine("");
+
+            Console.Write("Informe o capital inicial: R$");
+            double capitalInicial = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a taxa diária: ");
+            double taxaDiaria = double.Parse(Console.ReadLine());
+
+            Console.Write("Informe a data de inicio: ");
+            DateTime dataAtual = DateTime.ParseExact(Console.ReadLine(), "d", null);
+
+            Console.Write("Informe a data final: ");
+            DateTime dataFinal = DateTime.ParseExact(Console.ReadLine(), "d", null);
+
+            Investimento investimento = new Investimento(capitalInicial, taxaDiaria, dataAtual, dataFinal);
+            investimento.Investir();
+            investimento.MostrarInvestimentoFuturo();
+            return investimento;
+        }
+    }
+}
